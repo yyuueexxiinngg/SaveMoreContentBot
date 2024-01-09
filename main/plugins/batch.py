1fc7a34c72a46c1095ec4af9134e0c70620bd877
SamKingotw <155861386+SamKingotw@users.noreply.github.com>
2024-01-09 18:59:55 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -102,12 +102,11 @@ async def run_batch(userbot, client, sender, link, _range):
         try:
             await get_bulk_msg(userbot, client, sender, link, i) 
         except FloodWait as fw:
-            if int(fw.x) > 100000:
-                await client.send_message(sender, "Cancelling batch since you have floodwait more than 5 minutes.")
-                break
-            await asyncio.sleep(fw.x + 5)
+            await asyncio.sleep(fw.seconds + 5)
             await get_bulk_msg(userbot, client, sender, link, i)
         protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
-        await asyncio.sleep(timer)
+        time.sleep(timer)
         await protection.delete()
+
+
             
